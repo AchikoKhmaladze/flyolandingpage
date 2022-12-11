5b566388d3400d4ba7914350b03f1ab4315a94ff
khmalaa <119732582+khmalaa@users.noreply.github.com>
2022-12-11 11:39:47 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let markWeight = 78;
+let markHeight = 1.69;
+
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markbmi = markWeight / (markHeight * markHeight);
+let johnbmi = johnWeight / (johnHeight * johnHeight);
+
+if (markbmi > johnbmi) {
+    console.log(`markbmi ${markbmi}is higher than johns ${johnbmi} `)
+} else {
+    console.log(`johnsbmi (${johnbmi}) is higher than markbmi (${markbmi})`)
+}
+
+
+const age = 1995
+
+console.log(String(age), age);
+
+
+
+
+    // const firstName = "achi";
+// const job = "studenti";
+// const birthYear = 2003;
+// const year = 2040;
+
+// const achi = ` i'm ${firstName} , a ${year - birthYear} year old ${job} !! `;
+
+// console.log(achi);
+
+
